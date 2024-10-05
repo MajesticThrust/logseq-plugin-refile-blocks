4a9936eca5d263b77fc9aae7051c0ab093afb469
MajesticThrust <majesticthrust@gmail.com>
2024-10-05 22:47:53 +0200

    wip: modal and test command

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,54 @@
-import "./App.css";
+import { useState } from 'react'
+
+import './App.css'
 
 const App = () => {
+  const [blocks, setBlocks] = useState([
+    { id: 1, content: 'block 1' },
+    { id: 2, content: 'block 2' },
+    { id: 3, content: 'block 3' },
+  ])
+
+  const dismissModal = () => {
+    logseq.hideMainUI()
+  }
+
   return (
-    <div className="w-screen h-screen flex items-center justify-center text-white">
-      <div
-        className="w-screen h-screen fixed top-0 left-0"
-        onClick={() => logseq.hideMainUI()}
-      ></div>
-      <div className="w-5/6 h-5/6 z-0 bg-gradient-to-tr from-green-300 via-green-500 to-green-700 flex flex-col items-center justify-center">
-        <h1 className="font-bold text-4xl">logseq-plugin-refile-blocks</h1>
-        <h2 className="text-2xl mt-6">
-          Current Env: {import.meta.env.VITE_MODE}
-        </h2>
+    <div className="flex h-screen w-screen items-center justify-center" onClick={dismissModal}>
+      <div className="size-fit rounded border border-black bg-white">
+        {/* searchbar */}
+        {/* TODO mvp - plain text search (fuzzy) */}
+        {/* TODO optional - full datalog queries mode, enabled with checkbox */}
+        <div className="w-full rounded border-b">
+          <input type="text" placeholder="Where to refile selected block(s)?" className="bg-transparent p-1" />
+        </div>
+
+        {/* search result list */}
+        <div className="p-1">
+          {blocks.map((block) => (
+            <div key={block.id}>{block.content}</div>
+          ))}
+        </div>
       </div>
     </div>
-  );
-};
 
-export default App;
+    // <div className="w-screen h-screen flex items-center justify-center text-white">
+    //   <div
+    //     className="w-screen h-screen fixed top-0 left-0"
+    //     onClick={() => logseq.hideMainUI()}
+    //   ></div>
+    //   <div className="w-5/6 h-5/6 z-0 bg-gradient-to-tr from-green-300 via-green-500 to-green-700 flex flex-col items-center justify-center">
+    //     <h1 className="font-bold text-4xl">logseq-plugin-refile-blocks</h1>
+    //     <h2 className="text-2xl mt-6">
+    //       Current Env: {import.meta.env.VITE_MODE}
+    //     </h2>
+    //   </div>
+    // </div>
+  )
+}
+
+export default App
+
+function getAllBlocks() {
+  return logseq.Editor.getAllBlocks()
+}
--- src/main.tsx
+++ src/main.tsx
@@ -1,51 +1,85 @@
-import "@logseq/libs";
-import React from "react";
-import proxyLogseq from "logseq-proxy";
-import { type Root, createRoot } from "react-dom/client";
-import App from "./App";
-import "./index.css";
+import '@logseq/libs'
+import type { SettingSchemaDesc } from '@logseq/libs/dist/LSPlugin'
+import proxyLogseq from 'logseq-proxy'
+import React from 'react'
+import { type Root, createRoot } from 'react-dom/client'
 
-if (import.meta.env.VITE_MODE === "web") {
+import App from './App'
+import './index.css'
+
+const settings: SettingSchemaDesc[] = [
+  {
+    key: 'refile-test',
+    type: 'string',
+    title: 'Refile Test',
+    description: 'A test command for refiling blocks',
+    default: 'ctrl+alt+r',
+  },
+]
+
+if (import.meta.env.VITE_MODE === 'web') {
   // run in browser
-  console.log(
-    "[faiz:] === meta.env.VITE_LOGSEQ_API_SERVER",
-    import.meta.env.VITE_LOGSEQ_API_SERVER
-  );
-  console.log(
-    `%c[version]: v${__APP_VERSION__}`,
-    "background-color: #60A5FA; color: white; padding: 4px;"
-  );
+  console.log('meta.env.VITE_LOGSEQ_API_SERVER', import.meta.env.VITE_LOGSEQ_API_SERVER)
+  console.log(`%c[version]: v${__APP_VERSION__}`, 'background-color: #60A5FA; color: white; padding: 4px;')
   proxyLogseq({
     config: {
       apiServer: import.meta.env.VITE_LOGSEQ_API_SERVER,
       apiToken: import.meta.env.VITE_LOGSEQ_API_TOKEN,
     },
     settings: window.mockSettings,
-  });
-  renderApp();
+  })
+  renderApp()
 } else {
-  console.log("=== logseq-plugin-refile-blocks loaded ===");
-  logseq.ready(() => {
+  console.log('=== logseq-plugin-refile-blocks loaded ===')
+  logseq.useSettingsSchema(settings).ready(() => {
+    console.log('settings', logseq.settings)
+
     logseq.provideModel({
       show() {
-        renderApp();
-        logseq.showMainUI();
+        renderApp()
+        logseq.showMainUI()
       },
-    });
+    })
 
-    logseq.App.registerUIItem("toolbar", {
-      key: "logseq-plugin-refile-blocks",
-      template:
-        '<a data-on-click="show" class="button"><i class="ti ti-window"></i></a>',
-    });
-  });
+    // Close the modal by clicking ESC key
+    document.addEventListener(
+      'keydown',
+      function (e) {
+        if (e.key === 'Escape') {
+          logseq.hideMainUI(/*{ restoreEditingCursor: true }*/)
+        }
+        e.stopPropagation()
+      },
+      false,
+    )
+
+    logseq.App.registerUIItem('toolbar', {
+      key: 'logseq-plugin-refile-blocks',
+      template: '<a data-on-click="show" class="button"><i class="ti ti-window"></i></a>',
+    })
+
+    if (logseq.settings?.['refile-test']) {
+      logseq.App.registerCommandPalette(
+        {
+          key: 'refile-test',
+          label: 'Refile Test',
+          keybinding: { mode: 'global', binding: logseq.settings['refile-test'] as string },
+        },
+        () => {
+          // TODO find currently selected block or blocks
+          // TODO return if no blocks selected
+          console.log('Refile Test')
+        },
+      )
+    }
+  })
 }
 
 function renderApp() {
-  const root = createRoot(document.getElementById("root")!);
+  const root = createRoot(document.getElementById('root')!)
   root.render(
     <React.StrictMode>
       <App />
-    </React.StrictMode>
-  );
+    </React.StrictMode>,
+  )
 }
